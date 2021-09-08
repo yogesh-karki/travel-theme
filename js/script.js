bf866f68703ddede20d46d94faedc1cc5772133b
yogesh-karki <yogesh@hueshine.com>
2021-09-08 11:11:07 +0545

    homepage

--- js/script.js
+++ js/script.js
@@ -16,7 +16,6 @@ if(introWrapper) {
     const introMouseMove =  () => {
         introBtn.style.top = mouseY;
         introBtn.style.left = mouseX;
-        console.log("hello")
         window.requestAnimationFrame(introMouseMove);
     }
 
@@ -44,7 +43,122 @@ ScrollTrigger.scrollerProxy(".scrollContainer", {
 });
 
 
+gsap.to('.scrub-head h2',{
+  scrollTrigger: {
+    trigger: ".stories",
+    start: 'top center',
+    end: "bottom bottom",
+    scroller: '.scrollContainer',
+    scrub: 3,
+    },
 
+    x: 200
+})
+
+var tl = gsap.timeline({
+    scrollTrigger: {
+    trigger: ".footer-animations",
+    start: 'top top',
+    end: "bottom bottom",
+    scroller: '.scrollContainer',
+ 
+    scrub: 3,
+    pin: true,
+  }
+});
+
+tl.fromTo('.bottom-img',{
+  y: 0,
+},{
+  y: -150
+})
+
+tl.fromTo('.cloud-left',{
+  scrollTrigger: {
+    trigger: ".footer-animations",
+    start: 'top center',
+    end: "bottom bottom",
+    scrub: 3,
+    duration: 10
+  },
+
+  x: -100,
+  opacity: 0
+  
+},{
+  xPercent: 100,
+  scale: 1.2,
+  opacity: 1
+  
+},0)
+
+tl.fromTo('.cloud-right',{
+
+  x: 100,
+  opacity: 0.5
+  
+},{
+  xPercent: -100,
+  scale: 1.2,
+  opacity: 1
+  
+},0)
+
+tl.fromTo('.top-img',{
+  scale: 1
+  
+},{
+  scale: 1.3
+},0)
+
+gsap.to('.logo-text', {
+  scrollTrigger: {
+    trigger: ".footer-animations",
+    start: 'top top',
+    end: "bottom bottom",
+    scroller: '.scrollContainer',
+    scrub: 1
+  },
+  scale: 1.1,
+  y: -400
+})
+
+
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight
+
+gsap.utils
+  .toArray(".cloud-slides")
+  .forEach(($el) => {
+    const width = $el.clientWidth;
+    const delay =  2;
+    const repeatDelay = 1;
+    
+
+    gsap.set($el, {
+      opacity: 1,
+    });
+
+    gsap.fromTo(
+      $el,
+      {
+        xPercent: -50,
+        y: 200,
+        opacity: 0.5
+      },
+      {
+        duration: 15,
+        x: width,
+        y: -200,
+        repeat: -1,
+        delay,
+        repeatDelay,
+        ease: "none",
+        opacity: 1
+      }
+    );
+
+  })
 
 
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
